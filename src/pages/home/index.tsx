e7914a2e3f92d848fad4a83231835c8565d5070c
AishwaryVishwakarma <aishwaryvishwakarma23@gmail.com>
2023-05-21 01:13:31 +0530

    completed home hero

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Layout from '../../components/Layout/Layout';
 import styles from './styles.module.scss';
+import Typewriter from 'typewriter-effect';
 
 const HomePage: React.FC = () => {
   return (
@@ -9,15 +10,25 @@ const HomePage: React.FC = () => {
         <div className={styles.homeHero}>
           <div className={styles.leftContainer}>
             <div className={styles.heading}>
-              Hey there, I am <span>Aishwary Vishwakarma</span>
+              Hey there, I am{' '}
+              <span>
+                <Typewriter
+                  options={{
+                    strings: ['Aishwary Vishwakarma'],
+                    autoStart: true,
+                    loop: true,
+                    deleteSpeed: 10
+                  }}
+                />
+              </span>
             </div>
             <div className={styles.description}>
               After 2 years of coding and getting my hands dirty in development
               I realised that programmers actually don't code. They understand
               the requirements and convert them into code to shape the product.
               So I don't think in code, but rather I write code by thinking
-              about the impact. I really believe that my skills can be used to
-              scale a product and create an impact through it.
+              about the impact. I really believe that my skills can be used
+              toscale a product and create an impact through it.
             </div>
             <div className={styles.ctaContainer}>
               <a href='src/assets/Resume.pdf' download className={styles.cvCta}>
@@ -28,7 +39,9 @@ const HomePage: React.FC = () => {
               </a>
             </div>
           </div>
-          <div className={styles.rightContainer}></div>
+          <div className={styles.rightContainer}>
+            <img src='src/assets/Profile-picture.jpeg' alt='' />
+          </div>
         </div>
       </section>
     </Layout>
