7a2db78b65d97785a4903948314a4d44dc607273
AishwaryVishwakarma <aishwaryvishwakarma23@gmail.com>
2023-05-21 12:42:07 +0530

    added scroll effect for home hero

--- src/components/Pages/Home/HomeHero/index.tsx
+++ src/components/Pages/Home/HomeHero/index.tsx
@@ -1,51 +1,61 @@
 import React from 'react';
 import styles from './styles.module.scss';
 import Typewriter from 'typewriter-effect';
+import {Parallax, ParallaxProvider} from 'react-scroll-parallax';
 
 const HomeHero = () => {
   return (
     <div className='full-bleed layouted'>
-      <video autoPlay muted loop>
-        <source src='src/assets/Stars-video.mp4' type='video/mp4' />
-      </video>
-      <div className={styles.homeWrapper}>
-        <section id='home-hero' className={styles.homeHero}>
-          <div className={styles.leftContainer}>
-            <div className={styles.heading}>
-              Hey there, I am{' '}
-              <span>
-                <Typewriter
-                  options={{
-                    strings: 'Aishwary Vishwakarma',
-                    autoStart: true,
-                    loop: true,
-                    deleteSpeed: 100,
-                  }}
-                />
-              </span>
+      <ParallaxProvider>
+        <Parallax className={styles.videoContainer} speed={-200}>
+          <video autoPlay muted loop>
+            <source src='src/assets/Stars-video.mp4' type='video/mp4' />
+          </video>
+        </Parallax>
+        <div className={styles.homeWrapper}>
+          <section id='home-hero' className={styles.homeHero}>
+            <div className={styles.leftContainer}>
+              <div className={styles.heading}>
+                Hey there, I am{' '}
+                <span>
+                  <Typewriter
+                    options={{
+                      strings: 'Aishwary Vishwakarma',
+                      autoStart: true,
+                      loop: true,
+                      deleteSpeed: 100,
+                    }}
+                  />
+                </span>
+              </div>
+              <div className={styles.description}>
+                After 2 years of coding and getting my hands dirty in
+                development I realised that programmers actually don't code.
+                They understand the requirements and convert them into code to
+                shape the product. So I don't think in code, but rather I write
+                code by thinking about the impact. I really believe that my
+                skills can be used toscale a product and create an impact
+                through it.
+              </div>
+              <div className={styles.ctaContainer}>
+                <a
+                  href='src/assets/Resume.pdf'
+                  download
+                  className={styles.cvCta}
+                >
+                  Download CV
+                </a>
+                <a href='#contact' download className={styles.contactCta}>
+                  Let's Talk
+                </a>
+              </div>
             </div>
-            <div className={styles.description}>
-              After 2 years of coding and getting my hands dirty in development
-              I realised that programmers actually don't code. They understand
-              the requirements and convert them into code to shape the product.
-              So I don't think in code, but rather I write code by thinking
-              about the impact. I really believe that my skills can be used
-              toscale a product and create an impact through it.
+            <div className={styles.rightContainer}>
+              <img src='src/assets/Profile-picture.jpeg' alt='' />
             </div>
-            <div className={styles.ctaContainer}>
-              <a href='src/assets/Resume.pdf' download className={styles.cvCta}>
-                Download CV
-              </a>
-              <a href='#contact' download className={styles.contactCta}>
-                Let's Talk
-              </a>
-            </div>
-          </div>
-          <div className={styles.rightContainer}>
-            <img src='src/assets/Profile-picture.jpeg' alt='' />
-          </div>
-        </section>
-      </div>
+          </section>
+        </div>
+      </ParallaxProvider>
     </div>
   );
 };
