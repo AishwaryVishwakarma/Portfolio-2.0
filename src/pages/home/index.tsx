1e568838f53492b2e9f277b62a8bd4fd1502ca28
AishwaryVishwakarma <aishwaryvishwakarma23@gmail.com>
2023-05-26 01:44:01 +0530

    integrated sanity

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,12 +2,26 @@ import React from 'react';
 import Layout from '../../components/Layout/Layout';
 import HomeHero from '../../components/Pages/Home/HomeHero';
 import Projects from '../../components/Pages/Home/HomeProjects';
+import {client} from '../../client';
 
 const HomePage: React.FC = () => {
+  const [homeData, setHomeData] = React.useState();
+
+  React.useEffect(() => {
+    client
+      .fetch('*[_type == "homePage"] {homeHeroSection}')
+      .then((res) => {
+        setHomeData(res);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const {homeHeroSection}: any = (homeData && homeData[0]) ?? {};
+
   return (
     <Layout className='defaultBg full-bleed'>
       {/* Hero */}
-      <HomeHero />
+      <HomeHero heroData={homeHeroSection} />
       {/* Projects */}
       <Projects />
     </Layout>
