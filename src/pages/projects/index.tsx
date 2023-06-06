fe73355df94e51c0d54463645cbd48cc2b4bbbbd
AishwaryVishwakarma <aishwaryvishwakarma23@gmail.com>
2023-06-06 23:45:55 +0530

    resolved build issue

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import styles from './styles.module.scss';
 import Layout from '../../components/Layout/Layout';
-import {client} from '../../client';
 
 const ProjectsPage: React.FC = () => {
   return <Layout>hello</Layout>;
